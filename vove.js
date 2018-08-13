32f6a1eface4604c7c80fd7b51c6e32911945ca4
Paulo Henrique <gladgamer01@gmail.com>
2018-08-13 17:20:22 -0300

    Update vove.js

--- vove.js
+++ vove.js
@@ -1,35 +1,14 @@
-	
-		// EU VO VE E TE AVISO
-	var imgLink = "https://imgur.com/8jrEfw5.png";
-	
-	window.setInterval('imagensPostFace()', 500);
-	window.setInterval('imagensGeral()', 500);
-	window.setInterval('imagensPerfilFace()', 500);
+ChangeImages = () => {
+  const __VoVEEteAviso__ = 'https://imgur.com/8jrEfw5.png'
+  var object = document.getElementsByTagName("img");
+  setInterval(()=> {
+    for (var x = 0; x < object.length;x++){
+      if(object[x].getAttribute('src') != __VoVEEteAviso__){
+        object[x].setAttribute('src',__VoVEEteAviso__);
+      }
+    }
+  },1500); //1.5s
+}
 
-	function imagensGeral(){
-		var x = document.getElementsByTagName("img");
-		var i;
-			    
-		for (i = 0; i < x.length; i++) {
-			x[i].src = imgLink;
-		}
-	}
-	
-	function imagensPostFace(){
-		var x = document.getElementsByClassName("uiScaledImageContainer");
-		var i;
-			    
-		for (i = 0; i < x.length; i++) {
-			x[i].src = imgLink;
-		}
-	}
 
-	function imagensPerfilFace(){
-		var x = document.getElementsByClassName("_38vo");
-		var i;
-			    
-		for (i = 0; i < x.length; i++) {
-			x[i].src = imgLink;
-		}
-	}
-	
+ChangeImages();
